eedeb621f07a4cb15fbe3a7ec0e097e51d8ac0d0
arsaqqa <arsaqqa@nablus.org>
2026-02-11 11:18:19 +0200

    ttest

---
+++ RamadanQuiz/Models/EmplyeeAnswerQuestion.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RamadanQuiz.Models
+{
+    public class EmplyeeAnswerQuestion
+    {
+        [Key]
+        public int EmployeeAnswerId { get; set; }
+        [ForeignKey(nameof(Question))]
+        public int QuestionId { get; set; }
+
+        public Question Question { get; set; }
+
+        public int EmployeeId { get; set; }
+
+     public EmployeeAnswer  EmployeeAnswer { get; set; }
+        public int QuestionOptionId { get; set; }
+        public QuestionOption QuestionOption { get; set; }
+        public bool IsCorrect { get; set; }
+
+    }
+}
